0c98095267f6ee7e7c2cbff42e8e75b05579aa2c
Donna Torres <todonnatorres@icloud.com>
2020-06-06 19:02:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+    lowest_key = 
   
 
 end

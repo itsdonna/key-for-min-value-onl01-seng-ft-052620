de5dd4cce4391ddb156a4a159f480b9570685986
Donna Torres <todonnatorres@icloud.com>
2020-06-06 19:08:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(hash)
     lowest_value = nil
     hash.each do |key, value|
       if lowest_value = nil || value < lowest_value
-  
-
+        lowest_value = value
+        lowest_key = key 
+  end
 end
